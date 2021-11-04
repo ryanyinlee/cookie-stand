6bd91d23d22711f45c35a4b4ada8e8fcfc821f6a
Ryan Lee <ryanyinlee@protonmail.com>
2021-11-04 10:25:15 -0700

    Adding that bottom row.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function getRandomCust(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
+// Hours can be variable.
 let hours = ['6AM', '7AM', '8AM', '9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM', '6PM', '7PM']; // from emily
 // Shop Constructor
 function Cookieshop(cityName, minHourlyCust, maxHourlyCust, cookiesPerCust) {
@@ -42,7 +42,6 @@ Cookieshop.prototype.getTotalSales = function () {
     }
     return outputTotal;
 }
-
 
 function renderFooterRow() {
     // Add up hourly total
